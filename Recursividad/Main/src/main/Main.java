34d7ee66653d7524759966a157887683f10ba18d
Alfred024 <21030761tcelaya.edu.mx>
2022-08-10 22:37:26 -0500

    Acabado el método recursivo para sacar un Factorial

---
+++ Recursividad/Main/src/main/Main.java
@@ -0,0 +1,26 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int factorialX = 8;
+        System.out.print("Factorial de "+factorialX+": "+factorial(factorialX)+"\n");
+        
+    }
+    
+    //Calcular el factorial con métodos recursivos
+    static int factorial(int num){
+        int res = 1;
+        if(num > 1){
+          res = num * factorial(num-1);
+        }
+        return res;
+    }
+    
+    //Calcular secuencia fibonacci hasta la vuelta X
+    static int fibonacci(){
+        return 0;
+    }
+}
