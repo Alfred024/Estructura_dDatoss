74124374adb93d66ac118e44940579868d156e5b
Alfred024 <21030761tcelaya.edu.mx>
2022-08-10 19:07:26 -0500

    Creación de la estructura del Nodo y primeros métodos en la clase Linked List

--- Main/src/LinkedList.java
+++ Main/src/LinkedList.java
@@ -1,4 +1,44 @@
 
 public class LinkedList {
     
+    Nodo start, end;
+    
+    public LinkedList(){
+        this.start = null;
+        this.end = null;
+    }
+    
+    public void addStart(int value){
+        if(start == null){
+            Nodo newNode = new Nodo(value, null);
+            start = end = newNode;
+        }else{
+            Nodo newNode =new Nodo(value, start);
+            start = newNode;
+        }
+    }
+    
+    public void addEnd(int value){
+        Nodo newNode = new Nodo(value, null);
+        if(start == null){
+            start = end = newNode;
+        }else{
+            end.next = newNode;
+            end = newNode;
+        }
+    }
+    
+    public void showData(){
+        Nodo aux = start;
+        String lista="";
+        if(start != null){
+            while(aux.next != null){
+                lista+=aux.value+"--> ";
+                aux = aux.next;
+            }lista+=aux.value+"--> null";
+        }else{
+            System.out.println("Sin elementos en la lista");
+        }
+        
+    }
 }
